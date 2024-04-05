892a8d8f667fa93a3178ac450ba70eee44f40a75
Carlos Roberto Arias Arevalo <arias@spu.edu>
2024-04-05 11:01:57 -0700

    In process

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
+#include "vector.h"
+
 #include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::left;
+using std::setw;
+
+int Test(bool condition, const char *message);
+bool DoubleEquals(double a, double b, double epsilon = 1e-6);
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "LET\'S TEST IT" << endl;
+    double d1[] = {1,2,4};
+    double d2[] = {5, -1, 0};
+    double d3[] = {1,8};
+    double d4[] = {1, 2, 4};
+
+    Vector a(3, d1);
+    Vector b(3, d2);
+    Vector c(0, nullptr);
+    Vector d(2, d3);
+    Vector e(3, d4);
+
+    Test(a.ToString() == "[1, 2, 4]", "ToString");
+    c = a.Sum(b);
+    Test(c.ToString() == "", "Sum");
+
     return 0;
+}
+
+
+int Test(bool condition, const char *message) {
+    static auto passedTests = 0;
+    if (condition) {
+        cout << "TEST PASSED (" << left << setw(50) << message << ") Total Passed: " << ++passedTests << endl;
+    } else {
+        cout << "TEST FAILED (" << left << setw(50) << message << ") **************" << endl;
+    }
+    return passedTests;
+}
+bool DoubleEquals(double a, double b, double epsilon){
+    return abs(a - b) < epsilon;
 }
--- vector.cpp
+++ vector.cpp
@@ -4,6 +4,10 @@
 
 #include "vector.h"
 
+#include <cmath>
+#include <sstream>
+
+using std::stringstream;
 
 Vector::Vector(size_t size, double *data): _size(size) {
     _data = new double[_size];
@@ -37,29 +41,74 @@ double Vector::DotProduct(const Vector &rhs) const {
     double retVal = 0;
     if (_size != rhs._size)
         exit(1);
+    for (int i = 0; i < _size; ++i) {
+        retVal += _data[i] * rhs._data[i];
+    }
     return 0;
 }
 
 const Vector Vector::Sum(const Vector &rhs) const {
-    return Vector(0, nullptr);
+    if (_size != rhs._size)
+        exit(1);
+    double* tmp = new double[_size];
+    for (int i = 0; i <_size; ++i) {
+        tmp[i] = _data[i] + rhs._data[i];
+    }
+    Vector retVal(_size, tmp);
+    delete[] tmp;
+    return retVal;
 }
 
 const Vector Vector::Sub(const Vector &rhs) const {
-    return Vector(0, nullptr);
+    if (_size != rhs._size)
+        exit(1);
+    double* tmp = new double[_size];
+    for (int i = 0; i <_size; ++i) {
+        tmp[i] = _data[i] + rhs._data[i];
+    }
+    Vector retVal(_size, tmp);
+    delete[] tmp;
+    return retVal;
 }
 
 const Vector Vector::ScalarMultiplication(double value) const {
-    return Vector(0, nullptr);
+    Vector retVal(_size, _data);
+    for (int i = 0; i < _size; ++i) {
+        retVal._data[i] *= -1;
+    }
+    return retVal;
 }
 
 string Vector::ToString() const {
-    return std::string();
+    stringstream ss;
+    ss << "[";
+    for (int i = 0; i < _size; ++i) {
+        if (i != _size - 1) // not last
+            ss << _data[i] << ", ";
+        else
+            ss << _data[i] << "]";
+    }
+    return ss.str();
 }
 
 bool Vector::Equals(const Vector &rhs) const {
-    return false;
+    if (_size != rhs._size)
+        return false;
+    for (int i = 0; i < _size; ++i) {
+        if (_data[i] != rhs._data[i])
+            return false;
+    }
+    return true;
 }
 
 double Vector::Distance(const Vector &rhs) const {
-    return 0;
+    if (_size != rhs._size)
+        exit(1);
+
+    double sum = 0.0;
+    for (int i = 0; i < _size; ++i) {
+        sum += pow(_data[i] - rhs._data[i], 2.0);
+    }
+
+    return sqrt(sum);
 }
